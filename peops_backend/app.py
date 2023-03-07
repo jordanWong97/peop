b4f81d05da02d20724b380348475872a931bb5fd
Jordan Wong <pjwong97@gmail.com>
2023-03-06 19:12:31 -0800

    added react proxy, basic routing test

--- peops_backend/app.py
+++ peops_backend/app.py
@@ -1,3 +1,15 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'hello world'
+
+
+@app.route('/celebrities', methods=['GET', 'POST'])
+def celebrities():
+    if request.method == 'POST':
+        return post_data()
+    return {'celebrities': ['celeb1', 'celeb2', 'celeb3']}
